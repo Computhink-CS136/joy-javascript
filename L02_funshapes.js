8904d5a23a9e122265236728bf89aa102ba5cffc
Computhink-CS136 <cs136@computhink.com.sg>
2025-07-14 10:48:17 +0000

    Jul 14, 2025, 10:48 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -31,6 +31,7 @@ function draw() {
   ellipse(400, 200, 100, 50)
   triangle(380, 400, 100, 100)
   triangle(580, 400, 100, 100)
+  
   fill("black")
   // Challenge 6: Add some colours to the drawings in the above challenges
 
