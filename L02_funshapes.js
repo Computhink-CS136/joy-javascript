912d68369b0084f837c7d64f1c38e8ba0c4fed00
Computhink-CS136 <cs136@computhink.com.sg>
2025-07-14 10:56:01 +0000

    Jul 14, 2025, 10:56 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -31,7 +31,7 @@ function draw() {
   // ellipse(400, 200, 100, 50)
   triangle(380, 400, 100, 100)
   triangle(580, 400, 100, 100)
-  
+  triangle(900, 400, 100, 200) 
   
   circle(400, 300, 100, 50) 
   fill("purple")
