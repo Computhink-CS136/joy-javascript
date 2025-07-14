b45bd7751bf9af5939ae1dba54bd6d17fdda4842
Computhink-CS136 <cs136@computhink.com.sg>
2025-07-14 10:41:29 +0000

    Jul 14, 2025, 10:41 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -25,7 +25,7 @@ function draw() {
   // triangle(230, 175, 185, 420, 386, 250)
   // Challenge 5: A square face using quad() function
   quad(320,725,120,869,290,30)
-  circle(300, 200, 200, 100)
+  circle(300, 100, 100, 100)
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
