964a17a1987ad666d5f1b1750533bda5992528ba
Computhink-CS136 <cs136@computhink.com.sg>
2025-07-14 10:23:45 +0000

    Jul 14, 2025, 10:23 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -19,7 +19,7 @@ function draw() {
 
   // ellipse(300, 400, 300, 200)
   // Challenge 3: A house using rect() function
-  rect(300, 400, 200, 200)
+  rect(300, 100, 200, 200)
   // Challenge 4: A rocketship using triangle() function
 
   // Challenge 5: A square face using quad() function
