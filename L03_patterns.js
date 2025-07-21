c81016c2276c848aeae34d2f2c5136835f6aafa8
Computhink-CS136 <cs136@computhink.com.sg>
2025-07-21 11:07:23 +0000

    Jul 21, 2025, 11:07 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -1,25 +1,30 @@
-let colorX = 0
+let colorX = 0;
 function setup() {
     createCanvas(895, 400);
     background(220);
     // noLoop(); // Stops continuous drawing
-    colorX = 0
+    colorX = 0;
   }
   
 function draw() {
-  // Recap 1: Repeating Circles
-  let posX = 50;
-  let posY = 200;
-  let circleD = 90;
-  let colorB = 0
+//   // Recap 1: Repeating Circles
+//   let posX = 50;
+//   let posY = 200;
+//   let circleD = 90;
+//   let colorB = 0
 
-  for (let i=1; i<15; i++){
-    posX = posX + circleD + 10;
-    fill(colorB, 0, 225)
-    colorB = colorB + 35;
-    circle(posX, posY, circleD);
+//   for (let i=1; i<15; i++){
+//     posX = posX + circleD + 10;
+//     fill(colorB, 0, 225)
+//     colorB = colorB + 35;
+//     circle(posX, posY, circleD);
 
-  }
+//   }
+
+
+fill(colorX, 0, 225);
+circle(150, 150, 200);
+colorX = colorX + 1;
 
 
 
