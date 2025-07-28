2633bb1f92d962c941425be32d9c8711b06ee4ee
Computhink-CS136 <cs136@computhink.com.sg>
2025-07-28 10:33:46 +0000

    Jul 28, 2025, 10:33 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -5,22 +5,22 @@ function setup() {
     background(230);
 }
 
-function draw() {
-    let r = 0;
-    let g = 0;
-    let b = 0;
-    let xpos = 50;
-    let ypos = 50;
+// function draw() {
+//     let r = 0;
+//     let g = 0;
+//     let b = 0;
+//     let xpos = 50;
+//     let ypos = 50;
 
-    for(let i = 0; i < 6; i++) {
-        fill(r, g, b)
-        g = g + 50
-        circle(xpos, ypos, 50);
-        xpos = xpos + 50;
-        ypos = ypos + 50;
-    }
-
-
+//     for(let i = 0; i < 6; i++) {
+//         fill(r, g, b)
+//         g = g + 50
+//         circle(xpos, ypos, 50);
+//         xpos = xpos + 50;
+//         ypos = ypos + 50;
+//     }
 
 
-}//this must be the last line
+
+
+//this must be the last line
