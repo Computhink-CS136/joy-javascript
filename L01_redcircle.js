14168c2dddd3b9b21985f953b42dfd842dd4f351
Computhink-CS136 <cs136@computhink.com.sg>
2025-07-28 09:43:58 +0000

    Jul 28, 2025, 9:43 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -3,9 +3,8 @@ function setup(){
     background(220);
 }
 
-function draw(){
-    fill(255, 0, 0);
-    ellipse(300,200,200,200);
+// function draw(){
+//     fill(255, 0, 0);
+//     ellipse(300,200,200,200);
     
     // Todo: Challenge 3 Traffic Light
-}
