2897d4007525620285573e7119560bf634992a76
Computhink-CS136 <cs136@computhink.com.sg>
2025-08-11 10:42:38 +0000

    Aug 11, 2025, 10:42 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -39,7 +39,7 @@ function draw(){
     if (keyIsDown(DOWN_ARROW)){
         ypos = ypos + 5;
     }
-    ypos = constrain(ypos, 0, height);
+    ypos = constrain(ypos, 0+25, height-25);
 
     fill("pink");
     circle(xpos, ypos, 50);
