a4703460d62476ed8028ac31b3d9b30caf4ff7ea
Computhink-CS136 <cs136@computhink.com.sg>
2025-08-20 08:25:06 +0000

    Aug 20, 2025, 8:25 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -14,7 +14,7 @@ function setup(){
 }
 function draw() {}
     background("pink")
-    image(staticImage, xpos, ypos, )
+    image(staticImage, xpos, ypos, 110, 133)
 //     fill("white");
 //     rect(30, 30, 340, 340);
 
