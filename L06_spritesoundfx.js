360b4befdd7f228b6db181e775b9fc5c10a6029f
Computhink-CS136 <cs136@computhink.com.sg>
2025-08-20 08:22:15 +0000

    Aug 20, 2025, 8:22 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,6 +1,6 @@
 let soundEffect, bgMusic, staticImage;
-let xpos = 300;
-let ypos = 200;
+// let xpos = 300;
+// let ypos = 200;
 
 function preload() {
     soundEffect = loadSound("assets/pop.mp3")
@@ -12,32 +12,32 @@ function setup(){
     createCanvas(400, 400);
     background(220);
 }
-function draw() {
-    fill("white");
-    rect(30, 30, 340, 340);
+function draw() {}
+//     fill("white");
+//     rect(30, 30, 340, 340);
 
-    fill("black");
-    rect(xpos, ypos, 35, 35);
+//     fill("black");
+//     rect(xpos, ypos, 35, 35);
 
-    if (keyIsDown(RIGHT_ARROW)){
-        xpos = xpos + 5;
-    }
-    if (keyIsDown(LEFT_ARROW)){
-        xpos = xpos - 5;
-    }
-    xpos = constrain(xpos, 0+40, width-2*40);
+//     if (keyIsDown(RIGHT_ARROW)){
+//         xpos = xpos + 5;
+//     }
+//     if (keyIsDown(LEFT_ARROW)){
+//         xpos = xpos - 5;
+//     }
+//     xpos = constrain(xpos, 0+40, width-2*40);
 
-    if (keyIsDown(UP_ARROW)){
-        ypos = ypos - 5;
-    }
-    if (keyIsDown(DOWN_ARROW)){
-        ypos = ypos + 5;
-    }
-    // if (keyIsDown("30")){
-    //     fill("pink");
-    // }
-    ypos = constrain(ypos, 0+40, height-2*40);
-}
+//     if (keyIsDown(UP_ARROW)){
+//         ypos = ypos - 5;
+//     }
+//     if (keyIsDown(DOWN_ARROW)){
+//         ypos = ypos + 5;
+//     }
+//     // if (keyIsDown("30")){
+//     //     fill("pink");
+//     // }
+//     ypos = constrain(ypos, 0+40, height-2*40);
+// }
     
 
 
