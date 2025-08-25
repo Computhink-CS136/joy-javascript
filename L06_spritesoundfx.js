00f9f7102f1306acae493b21d78ea44397f9c87d
Computhink-CS136 <cs136@computhink.com.sg>
2025-08-25 11:02:13 +0000

    Aug 25, 2025, 11:02 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -59,7 +59,7 @@ function draw() {
     if (keyIsDown(85)){
         xpos = xpos + 5;
     }
-    if (keyIsDown(8)){
+    if (keyIsDown(68)){
         xpos = xpos - 5;
     }
     xpos = constrain(xpos, 0, width-110);
