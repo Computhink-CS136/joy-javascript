4d164e4dafe856bd21ca8333350b0d43fb521c01
Computhink-CS136 <cs136@computhink.com.sg>
2025-09-01 10:00:40 +0000

    Sep 1, 2025, 10:00 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -9,7 +9,7 @@ function preload() {
     bgMusic = loadSound("assets/bossaNova.mp3")
     staticImage = loadImage("assets/pico-a.png")
     secondguy = loadImage('assets/penguin2-a.svg')
-    starfish = loadImage("")
+    starfish = loadImage("assets/")
 }
 
 function setup(){
