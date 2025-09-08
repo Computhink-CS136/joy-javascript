6cf5f3e6132b49570459db70f1779cc85aa74fe5
Computhink-CS136 <cs136@computhink.com.sg>
2025-09-08 09:59:07 +0000

    Sep 8, 2025, 9:59 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -20,7 +20,7 @@ function draw() {
 
     if (x > 400-25){
         speedX = speedX * (-1) * (1.1)
-        fill( random(0,255))
+        fill( random(0,255), random())
     }
     if (x < 0+25){
         speedX = speedX * (-1) * (1.1)
