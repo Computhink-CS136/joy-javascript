b022b2630218509f4c69d0b0d63688213e67631d
Computhink-CS136 <cs136@computhink.com.sg>
2025-09-08 10:44:56 +0000

    Sep 8, 2025, 10:44 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -15,7 +15,7 @@ function setup() {
     text("Choose:", 20, 150)
     text("'Read a book'  'Play a game'", 20, 200)
 
-    text("I am ")
+    text("I am Chicken ")
 
     // let xpos = 100;
     // for (let count=0; count<5; count++){
