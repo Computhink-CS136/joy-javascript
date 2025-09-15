0544dc057fc70ac21841b8a5188e5cc2b049f1ad
Computhink-CS136 <cs136@computhink.com.sg>
2025-09-15 10:27:02 +0000

    Sep 15, 2025, 10:27 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -25,7 +25,7 @@ function draw() {
     fill("magenta");
     textSize(40);
     textAlign(CENTER, CENTER);
-    text("Welcome", 0, 0);
+    text("The ", 0, 0);
     text("My name is Joy", 0, 50);
     ypos = ypos - 1;
 }
