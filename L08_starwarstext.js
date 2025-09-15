65c4363289edc74dd9e4d80551c08a4517828730
Computhink-CS136 <cs136@computhink.com.sg>
2025-09-15 10:29:43 +0000

    Sep 15, 2025, 10:29 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -23,7 +23,7 @@ function draw() {
 
     let Flames = [
         "Flames have never been a joke.",
-        "Young Mei had been cursed with the 'Night Flame' since she was born in "
+        "Young Mei had been cursed with the 'Night Flame' since she was born."
     ]
 
     translate( width/2, ypos)
