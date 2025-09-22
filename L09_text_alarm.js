669cffa4d2f88cec01c107b48acf07d42c78e018
Computhink-CS136 <cs136@computhink.com.sg>
2025-09-22 10:08:09 +0000

    Sep 22, 2025, 10:08 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -8,5 +8,17 @@ function setup() {
 }
 
 function draw() {
-
+    
     }
+    // background("pink");
+    // fill("gray");
+    // textAlign(CENTER, BOTTOM);
+
+    // textSize(32);
+    // text("Bounce!", xpos, height/2);  
+    // xpos = xpos + velocityX;
+    // if (xpos > width - 80){
+    //     velocityX = velocityX * -1;
+    // }
+    // else if (xpos < 0 + 80){
+    //         velocityX = velocityX * -1;
