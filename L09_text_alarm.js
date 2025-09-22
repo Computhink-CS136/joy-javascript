76a1484a95f01af80cfe2c8b5ca4b4cb640d4f44
Computhink-CS136 <cs136@computhink.com.sg>
2025-09-22 09:44:53 +0000

    Sep 22, 2025, 9:44 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,11 +1,11 @@
 function setup() {
     createCanvas(600, 400)
     background("pink")
-    
+
     textAlign(CENTER, BOTTOM)
-    textColor("cyan")
+    textColor("cyan");
     textSize(32);
-    text("Bounce!", 300, 200)
+    text("Bounce!", 300, 200);
 }
 
 function draw() {
