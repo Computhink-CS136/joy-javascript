2b76504779449d9389c05008aa3012137b5488ae
Computhink-CS136 <cs136@computhink.com.sg>
2025-09-22 10:25:31 +0000

    Sep 22, 2025, 10:25 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -15,7 +15,7 @@ function draw() {
     hour24 = nf(hour24, 2);
 
     let nowMinute = minute();
-    nowMinute()
+    nowMinute = nf()
     let aSecond  = second();
     textSize(32)
     textAlign(CENTER, CENTER)
