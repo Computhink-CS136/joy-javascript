908f167f03b41fd1b46100780cf35ae653a929ed
Computhink-CS136 <cs136@computhink.com.sg>
2025-10-06 10:43:44 +0000

    Oct 6, 2025, 10:43 AM

--- L10_name.js
+++ L10_name.js
@@ -23,11 +23,11 @@ function setup() {
     userInput.position(width/2, height/2 - 110)
     userInput.input(updateName);
 
-    ageInput = createInput(age, "date"); //date, time, text, number, password
+    ageInput = createInput(age);
     ageInput.position(width/2, height/2 - 50);
     ageInput.input(updateAge);
 
-    birthInput = createInput(birth, "date");
+    birthInput = createInput(birth, "date"); //date, time, text, number, password
     birthInput.position(width/2, height/2 - 50);
     birthInput.input(updateBirth);
 }
