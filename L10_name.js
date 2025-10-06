ebd1bfc55235a618888c9ebc3fd1418d6e8c7760
Computhink-CS136 <cs136@computhink.com.sg>
2025-10-06 10:26:54 +0000

    Oct 6, 2025, 10:26 AM

--- L10_name.js
+++ L10_name.js
@@ -37,7 +37,7 @@ function draw() {
     textSize(40);
     textAlign( LEFT,CENTER);
     text(username, 150, 100);
-    text(age, )
+    text(age, 150, 200)
 
     textAlign(RIGHT,CENTER);
     textSize(20);
