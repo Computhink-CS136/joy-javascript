90e5be92a6652c7e54525c6c462617475cafbf42
Computhink-CS136 <cs136@computhink.com.sg>
2025-10-06 09:59:24 +0000

    Oct 6, 2025, 9:59 AM

--- L10_name.js
+++ L10_name.js
@@ -13,7 +13,7 @@ function setup() {
 
     userInput = createInput(username);
     userInput.position(width/2 - 100, height/2)
-    userInput.input
+    userInput.input(ubdate)
 }
 
 function draw() {
