58c640e3c1d8e052be8e271375364632bd9934e9
Computhink-CS136 <cs136@computhink.com.sg>
2025-10-13 10:24:31 +0000

    Oct 13, 2025, 10:24 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -25,6 +25,12 @@ function setup() {
     sometextInput.position(width/2, height/2 - 260);
     sometextInput = createInput("e.g. play");
     sometextInput.position(width/2, height/2 - 210);
+    sometextInput = createInput("e.g. a cat");
+    sometextInput.position(width/2, height/2 - 260);
+    sometextInput = createInput("e.g. a cat");
+    sometextInput.position(width/2, height/2 - 260);
+    sometextInput = createInput("e.g. a cat");
+    sometextInput.position(width/2, height/2 - 260);
 }
 
 
