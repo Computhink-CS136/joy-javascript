0e7301820fe6074e0e1323c348112560cbffc8e8
Computhink-CS136 <cs136@computhink.com.sg>
2025-10-13 09:47:38 +0000

    Oct 13, 2025, 9:47 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -6,7 +6,7 @@ function setup() {
     button1 = createButton("Click on me😀")
     button1.position(250, 200)
     button1.size(110, 50)
-    rect(250, 250, 250, 80, 30)
+    rect(150, 250, 250, 80, 30)
 
 }
 
