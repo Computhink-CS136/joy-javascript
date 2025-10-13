f78d2c8a02bd016bf6eeea29813781a65912b906
Computhink-CS136 <cs136@computhink.com.sg>
2025-10-13 09:42:00 +0000

    Oct 13, 2025, 9:42 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -3,7 +3,7 @@ function setup() {
     createCanvas(600, 400)
     background("pink")
 
-    button1 = createButton("Click on me")
+    button1 = createButton("Click on me😀")
 }
 
 function draw() {
