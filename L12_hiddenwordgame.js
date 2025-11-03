38bbac44c136e00f63271f9e707e7d4047669ea5
Computhink-CS136 <cs136@computhink.com.sg>
2025-11-03 10:07:45 +0000

    Nov 3, 2025, 10:07 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -9,13 +9,6 @@ function draw() {
     createCanvas(1510, 685);
     background("pink");
 
-    
-    userInput = createInput(username);
-    userInput.position(width/2 - 150, height/2 - 105)
-    userInput.input(updateName);
-
-    function updateName () {
-    username = userInput.value();
 
     button1 = createButton("Click on me😀")
     button1.position(width/2, height/2)
@@ -24,5 +17,4 @@ function draw() {
 
     sometextInput = createInput("e.g. a cat");
     sometextInput.position(width/2, height/2 - 260);
-}
 }
