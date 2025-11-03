e081362b21a57512d32a44321e9bb83781d70891
Computhink-CS136 <cs136@computhink.com.sg>
2025-11-03 10:25:32 +0000

    Nov 3, 2025, 10:25 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -10,7 +10,7 @@ function setup() {
     guessButton = createButton("Guess");
     guessButton.position(width/2 + 100, height/2);
     guessButton.style("font-size", "20px")
-    guessB
+    guessButton.style("")
 }
 
 function draw() {
