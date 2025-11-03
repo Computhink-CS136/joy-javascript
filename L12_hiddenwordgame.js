0ea2853b8cd64d5d441ef9091666efa38d200e5c
Computhink-CS136 <cs136@computhink.com.sg>
2025-11-03 10:49:29 +0000

    Nov 3, 2025, 10:49 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -30,7 +30,7 @@ function setup() {
     generateHints();
 }
 function generateHints() {
-    hints = selected[0] + " " + "_ ".reapeat()
+    hints = selected[0] + " " + "_ ".reapeat(selected.length)
 }
 
 function checkAnswer() {
