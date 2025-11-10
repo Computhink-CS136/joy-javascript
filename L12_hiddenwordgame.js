f95909dcbf9b87046613012681b0808b6e9bf475
Computhink-CS136 <cs136@computhink.com.sg>
2025-11-10 10:01:43 +0000

    Nov 10, 2025, 10:01 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -47,7 +47,7 @@ function draw() {
 
     text("Guess the hidden 5-letter word!", width/2, 100);
     text("Attempts : 0", width/2, 150);
-    text("Hints: I _ _ E _", width/2, 200)
+    text("Hints: "+hints, width/2, 200)
     text("Subscribe to 'joy_penguin@codewhiz'!", width/2, height/2 + 150)
 }
     // button = createButton("Guess🤔")
