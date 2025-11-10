99013784fe2038b11c3c29be50b740eb1d06241c
Computhink-CS136 <cs136@computhink.com.sg>
2025-11-10 10:05:29 +0000

    Nov 10, 2025, 10:05 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -49,6 +49,7 @@ function draw() {
     text("Attempts : 0", width/2, 150);
     text("Hints: "+hints, width/2, 200)
     // text("Subscribe to 'joy_penguin@codewhiz'!", width/2, height/2 + 150)
+    
     fill("cyan");
     textSize(24);
     text(message, width/2, height/2 + 100);
