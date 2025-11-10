3ed3214e471e571eb0cf6665f1e421df92ff48d6
Computhink-CS136 <cs136@computhink.com.sg>
2025-11-10 10:09:51 +0000

    Nov 10, 2025, 10:09 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -36,7 +36,7 @@ function generateHints() {
 }
 
 function checkAnswer() {
-    let userInput = textInput
+    let userInput = textInput.value().
     if (textInput.value() === selected)
     message = "Guessed right! The hidden word is 'index' !";
 }
